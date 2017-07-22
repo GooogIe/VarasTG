24edd0d1ee33623a7fbd7a7838c18a624f979404
A Sad Loners <nameless5@protonmail.com>
2017-07-22 21:38:57 +0200

    Added a few things

--- plugins/chat.py
+++ plugins/chat.py
@@ -18,16 +18,19 @@ class Chat(Plugin):
         Plugin.__init__(self,"chat","Returns a list of informations about che current chat.","A Sad Loners",1.0)
     
     def run(self):
-	infos = "Hi "+self.user["first_name"]+" these are some chat's informations:\n"
-	infos += "Id: "+str(self.chat["id"])+"\n"
-	if self.chat["type"] == "group":
-		infos += "Title: "+self.chat["title"]+"\n"
-		infos += "Type: Group\n"
-		if self.chat["all_members_are_administrators"]:
-			infos += "Administrators: Everyone"
-		else:
-			infos += "Administrators"
-	if self.chat["type"] == "supergroup":
-		infos += "Title: "+self.chat["title"]+"\n"
-		infos += "Type: Supergroup\n"
-	return infos
+		infos = "Hi "+self.user["first_name"]+" these are some chat's informations:\n"
+		infos += "Id: "+str(self.chat["id"])+"\n"
+		if self.chat["type"] == "group":
+			infos += "Title: "+self.chat["title"]+"\n"
+			infos += "Type: Group\n"
+			if self.chat["all_members_are_administrators"]:
+				infos += "Administrators: Everyone"
+			else:
+				infos += "Administrators"
+		elif self.chat["type"] == "supergroup":
+			infos += "Title: "+self.chat["title"]+"\n"
+			infos += "Type: Supergroup\n"
+		elif self.chat["type"] == "private":
+			infos += "Username: "+self.chat["username"]+"\n"
+			infos += "Type: Private"
+		return infos
