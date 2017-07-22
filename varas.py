87fc7565b11391c642859d891ce7d75f775003b1
A Sad Loners <nameless5@protonmail.com>
2017-07-22 18:16:10 +0200

    Forgot to edit this, updated now

--- varas.py
+++ varas.py
@@ -4,34 +4,14 @@
 """
 Telegram bot, with a simple plugin system.
 Read exampleplugin.py on more infos on how to create one.
-Author(s): Habb0n and Neon
-Last modified: March 2017
-Version: 1.0
+Author(s): A Sad Loners and Neon
+Last modified: June 2017
+Version: 1.1
 Github: https://github.com/GooogIe and https://github.com/neon-loled/
 """
-
-import bot,utils
-from os.path import isfile
-
-
+from bot import varasBot,utils
 def main():
-    											#Initializing bot instance
-    if isfile("config.vrs"):								#If there is a config load settings from there
-        configs = utils.loadfile("config.vrs")
-        name = configs["Name"]
-        version = configs["Version"]
-        debug = configs["Debug"]
-        token = configs["Token"]
-        varas = bot.Bot(token,name,version,debug)
-    else:										#Otherwise ask for a new token
-    	utils.logaction("Type the bot Token: ")
-    	token = raw_input("")
-    	varas = bot.Bot(token)
-        
-    utils.info()
-    varas.saveConfig()
-    varas.loadPlugins()
-    varas.getUpdates()
-    
+    varas = varasBot.Bot()
+    varas.startCli()
 if __name__ == "__main__":
 	main()
